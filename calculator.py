222de0a6fc23a90eb280211cbd8ce32a37d272d3
Bulat Khasanov <afti@yandex.ru>
2015-11-13 02:16:00 +0300

    wrap Calculator params with Decimal and change some tests to almostEquals

--- calculator.py
+++ calculator.py
@@ -1,22 +1,35 @@
 import math
+from decimal import *
+
+
+def round_result_of_binary_operation(function):
+    def function_wrapper(a, b):
+        with localcontext(Context(8)):
+            return float(function(a, b))
+
+    return function_wrapper
 
 
 class Calculator():
     @staticmethod
+    @round_result_of_binary_operation
     def add(a, b):
-        return a + b
+        return Decimal(a) + Decimal(b)
 
     @staticmethod
+    @round_result_of_binary_operation
     def subtract(a, b):
-        return a - b
+        return Decimal(a) - Decimal(b)
 
     @staticmethod
+    @round_result_of_binary_operation
     def multiply(a, b):
-        return a * b
+        return Decimal(a) * Decimal(b)
 
     @staticmethod
+    @round_result_of_binary_operation
     def divide(a, b):
-        return float(a) / b
+        return Decimal(a) / Decimal(b)
 
     @staticmethod
     def sin(x):
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -22,10 +22,10 @@ class CalculatorTestCase(unittest.TestCase):
         self.assertAlmostEqual(self.calculator.add(3.2, 5), self.calculator.add(5, 3.2))
 
     def test_add_of_floats(self):
-        self.assertEqual(self.calculator.add(5.2, 1.2), 6.4)
+        self.assertAlmostEqual(self.calculator.add(0.1, 0.2), 0.3)
 
     def test_commutativity_of_add_of_floats(self):
-        self.assertEqual(self.calculator.add(1.2, 5.23), self.calculator.add(5.23, 1.2))
+        self.assertAlmostEqual(self.calculator.add(1.2, 5.23), self.calculator.add(5.23, 1.2))
 
     def test_negative_add(self):
         self.assertNotEqual(self.calculator.add(3, 4), -7)
@@ -39,7 +39,7 @@ class CalculatorTestCase(unittest.TestCase):
         self.assertAlmostEqual(self.calculator.subtract(5, 3.22), 1.78)
 
     def test_subtract_of_floats(self):
-        self.assertEqual(self.calculator.subtract(5.3, 1.2), 4.1)
+        self.assertAlmostEqual(self.calculator.subtract(5.3, 1.2), 4.1)
 
     def test_negative_subtract(self):
         self.assertNotEqual(self.calculator.subtract(4, 3), -1)
@@ -53,16 +53,16 @@ class CalculatorTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.multiply(2, 5), self.calculator.multiply(5, 2))
 
     def test_multiply_of_int_and_float(self):
-        self.assertAlmostEqual(self.calculator.multiply(5, 3.21), 16.05)
+        self.assertAlmostEqual(self.calculator.multiply(12, 0.1), 1.2)
 
     def test_commutativity_of_multiply_of_int_and_float(self):
         self.assertAlmostEqual(self.calculator.multiply(3.2, 5), self.calculator.multiply(5, 3.2))
 
     def test_multiply_of_floats(self):
-        self.assertEqual(self.calculator.multiply(5.2, 1.2), 6.24)
+        self.assertAlmostEqual(self.calculator.multiply(5.2, 1.2), 6.24)
 
     def test_commutativity_of_multiply_of_floats(self):
-        self.assertEqual(self.calculator.multiply(1.2, 5.23), self.calculator.multiply(5.23, 1.2))
+        self.assertAlmostEqual(self.calculator.multiply(1.2, 5.23), self.calculator.multiply(5.23, 1.2))
 
     def test_negative_multiply(self):
         self.assertNotEqual(self.calculator.multiply(3, 4), -7)
@@ -70,13 +70,13 @@ class CalculatorTestCase(unittest.TestCase):
     """Divide tests"""
 
     def test_divide_of_ints(self):
-        self.assertEqual(self.calculator.divide(5, -2), -2.5)
+        self.assertAlmostEqual(self.calculator.divide(5, -2), -2.5)
 
     def test_divide_of_int_and_float(self):
         self.assertAlmostEqual(self.calculator.divide(5, 3.2), 1.5625)
 
     def test_divide_of_floats(self):
-        self.assertAlmostEqual(self.calculator.divide(5.4, 1.2), 4.5)
+        self.assertAlmostEqual(self.calculator.divide(0.3, 0.1), 3)
 
     def test_divide_by_zero(self):
         self.assertRaises(ZeroDivisionError, self.calculator.divide, 3, 0)
@@ -87,13 +87,15 @@ class CalculatorTestCase(unittest.TestCase):
     """Sin tests"""
 
     def test_sin_of_zero(self):
-        self.assertEqual(self.calculator.sin(0), 0)
+        self.assertAlmostEqual(self.calculator.sin(0), 0)
 
     def test_sin_of_pi_div_2(self):
-        self.assertEqual(self.calculator.sin(math.pi/2), 1)
+        self.assertAlmostEqual(self.calculator.sin(math.pi / 2), 1)
 
-    def test_sin_of_floats(self):
-        self.assertLess(self.calculator.sin(5.4), 1)
+    def test_sin_range(self):
+        result = self.calculator.sin(5.4)
+        self.assertLessEqual(result, 1)
+        self.assertGreaterEqual(result, -1)
 
     def test_negative_sin(self):
-        self.assertNotAlmostEqual(self.calculator.sin(math.pi/6), 1)
+        self.assertNotAlmostEqual(self.calculator.sin(math.pi / 6), 1)
