4f6f62a6b161dc94994ac4e98f0231e5e502746d
Bulat Khasanov <afti@yandex.ru>
2015-11-13 21:57:48 +0300

    change type of Decimal arg from float to str, remove "almost" word from tests

--- calculator.py
+++ calculator.py
@@ -4,13 +4,17 @@ from decimal import *
 
 def round_result(function):
     def function_wrapper(*args):
+        args = tuple(map(str, args))
         with localcontext(Context(10)):
             return float(round(function(*args), 10))
 
     return function_wrapper
 
 
-class Calculator():
+class Calculator:
+    def __init__(self):
+        pass
+
     @staticmethod
     @round_result
     def add(a, b):
@@ -34,4 +38,4 @@ class Calculator():
     @staticmethod
     @round_result
     def sin(x):
-        return Decimal(math.sin(x))
+        return Decimal(math.sin(Decimal(x)))
